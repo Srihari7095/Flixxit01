f7ef51f10ae0596180ec5faa2d04ac91a9a54cd4
Srihari7095 <srihari7095@gmail.com>
2024-01-11 21:42:53 +0530

    updating

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://flixxit01-frontend.vercel.app", "http://localhost:3000"],
     credentials: true,
   })
 );
