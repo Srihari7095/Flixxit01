860a3bfce8ef89f4f96f06b40d2d95bf387fe8cd
Srihari7095 <114302327+Srihari7095@users.noreply.github.com>
2024-01-14 16:48:19 +0530

    Update razorpay.config.js

--- server/src/razorpay.config.js
+++ server/src/razorpay.config.js
@@ -1,9 +1,9 @@
 import razorpay from "razorpay";
-
-const razorpayKey = process.env.RAZORPAY_KEY_ID;
-const razorpaySecret = process.env.RAZORPAY_SECRET_KEY;
-
-export const razorpay = new Razorpay({
+let razorpay = new Razorpay({
+    key: process.env.RAZORPAY_KEY_ID,
+    secret: process.env.RAZORPAY_SECRET_KEY
+});
+ razorpay = new Razorpay({
   key_id: 'rzp_test_mGKdNrKBsxcHmC',
   key_secret: 'poAZ8QO2dAPWig1QKoJxDOpX',
 });
